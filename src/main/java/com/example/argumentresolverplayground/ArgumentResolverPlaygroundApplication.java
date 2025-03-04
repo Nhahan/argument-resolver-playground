61893aee7b416671e841ad0410e1fbf570cf4c9f
Nhahan <kisy324@naver.com>
2025-03-04 12:54:15 +0900

    init

---
+++ src/main/java/com/example/argumentresolverplayground/ArgumentResolverPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.argumentresolverplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgumentResolverPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgumentResolverPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/argumentresolverplayground/ArgumentResolverPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.argumentresolverplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgumentResolverPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
